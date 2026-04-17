4f1219e661adbb79aad49cf32ba1789f87180d24
Rodrigo Costa <rodrigojesusdacosta@gmail.com>
2026-04-17 17:29:14 -0300

    style: add font and font variable

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,9 @@ function App() {
 
     <>
 
-    <div className="w-40 h-40 bg-neutral-very-dark-desaturated-blue"></div>
-  
+      <div className="w-40 h-40 bg-neutral-very-dark-desaturated-blue"></div>
+
+      <p className="text-9xl text-bright-blue">Teste de fonte</p>
     </>
 
   )
