7b8610488a18728247b5ccfc4f3e18b577ff222e
Rodrigo Costa <rodrigojesusdacosta@gmail.com>
2026-04-20 19:48:03 -0300

    feat: add todos array

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 import { TodoHeader } from "./components/TodoHeader"
 import { TodoForm } from "./components/TodoForm"
 
+const todos = [
+  { id: 1, text: 'Todo 1' },
+  { id: 2, text: 'Todo 2' },
+  { id: 3, text: 'Todo 3' }
+]
+
 function App() {
 
   return (
@@ -9,11 +15,27 @@ function App() {
 
       <div className="bg-[url('/images/bg-desktop-dark.jpg')] h-80 bg-cover bg-center">
 
-        <div className="max-w-[43.75rem] m-auto p-2">
+        <div className="max-w-[43.75rem] m-auto p-8">
 
           <TodoHeader></TodoHeader>
 
           <TodoForm></TodoForm>
+
+          <div className="bg-neutral-very-dark-desaturated-blue rounded-md">
+            <ul>
+              {todos.map((todo) => (
+                <li className="p-6 border-b border-neutral-very-dark-grayish-blue" key={todo.id}>
+                  <div className="flex items-center gap-4">
+                    <button className="w-6 h-6 border rounded-full cursor-pointer border-neutral-dark-grayish-blue"></button>
+
+                    <p className="text-neutral-very-light-grayish-blue">{todo.text}</p>
+                  </div>
+                </li>
+              ))}
+
+            </ul>
+          </div>
+
 
         </div>
 
