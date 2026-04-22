d3f660da813a9e6e0d9cecb70dd2a077dd53535c
Rodrigo Costa <rodrigojesusdacosta@gmail.com>
2026-04-22 16:17:39 -0300

    refactor: refactor components

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,21 @@ import { TodoHeader } from "./components/TodoHeader"
 import { TodoForm } from "./components/TodoForm"
 import { TodoList } from "./components/TodoList"
 
-import { useContext } from "react"
-import { themeConfig } from "./contexts/theme"
-import { ThemeContext } from "./contexts/ThemeContext"
+import { TodoContainer } from "./components/TodoContainer"
 
 function App() {
 
-  const { theme } = useContext(ThemeContext)
-
   return (
 
+    <TodoContainer>
 
-    <>
       <TodoHeader></TodoHeader>
 
       <TodoForm></TodoForm>
 
       <TodoList></TodoList>
-    </>
 
-
+    </TodoContainer>
 
   )
 }
